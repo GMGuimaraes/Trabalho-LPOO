a72a13220ecf8d99de9293ae50c980710cc5eaf7
GMGuimaraes <62194953+GMGuimaraes@users.noreply.github.com>
2020-05-06 20:01:16 -0400

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,39 @@
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private char sexo;
+	
+	public Pessoa() {
+		super();
+		this.nome = "NaoDeclarado";
+		this.cpf = "000000000-00";
+		this.sexo = 1;
+	}
+	
+	public Pessoa(String nome, String cpf, char sexo) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.sexo = sexo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	
+}
+
