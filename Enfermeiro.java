94b52915d9ae6f30fb200bd453cb8eab94ca362f
GMGuimaraes <62194953+GMGuimaraes@users.noreply.github.com>
2020-05-06 21:43:30 -0400

    Update Enfermeiro.java

--- Enfermeiro.java
+++ Enfermeiro.java
@@ -1,4 +1,8 @@
-
 public class Enfermeiro extends Funcionario {
 	private int numeroRegistro;
+
+	public Enfermeiro(String nome, String cpf, char sexo, int id, int nreg){
+		super(nome, cpf, sexo, id);
+		this.numeroRegistro = nreg;
+	}
 }
