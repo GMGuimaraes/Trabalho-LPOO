f518607fd9e548106faa055a6b59673ab6d6498c
GMGuimaraes <62194953+GMGuimaraes@users.noreply.github.com>
2020-05-06 22:03:40 -0400

    Update Atendente.java

--- Atendente.java
+++ Atendente.java
@@ -1,4 +1,17 @@
 
 public class Atendente extends Funcionario {
 
-}
+	public Atendente(String nome, String cpf, char sexo, int id){
+		super(nome, cpf, sexo, id);
+	}
+	public void imprimeProntuario(Prontuario pront){
+		pront.exibeProntuario();
+	}
+	public boolean ConsultaCadastro(Pessoa people){
+		if(people.getCpf().equals("XXXXXXXXX-XX") || people.getCpf().isEmpty()){
+			return false;
+		}else{
+			return true;
+		}
+	}
+}	
